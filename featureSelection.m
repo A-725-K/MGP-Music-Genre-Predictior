f80f25de666446160a4e4b11413b886d492a0ab6
Kennedy12 <kennedy12@a725k>
2019-01-21 17:19:42 +0100

    Added a simple algorithm for feature selection

---
+++ featureSelection.m
@@ -0,0 +1,42 @@
+%% select a subset of features of the dataset
+function I = featureSelection(X, N)
+    [rows, cols] = size(X);
+    step = rows/N;
+    
+    S = zeros(N, cols-1);
+    for j = 1:cols-1
+        for i = 1:N
+            c = (i-1)*step+1;
+            mean_c = mean(X([c:c+step-1], j));
+            mean_others = mean(X([1:c-1 c+step-1:end], j));
+            var_c = var(X([c:c+step-1], j));
+            var_others = var(X([1:c-1 c+step-1:end], j));
+            S(i, j) = fisherCriterion(mean_c, mean_others, var_c, var_others);
+        endfor
+    endfor
+    S_means = mean(S(:, :));
+    
+    I = selectColumns(S_means, 0.01);
+endfunction
+
+%% to apply the Fisher's criterion to evaluate the goodness
+function n = fisherCriterion(mean_c, mean_others, var_c, var_others)
+    num = (mean_c - mean_others);
+    den = sqrt(var_c + var_others);
+    n = num/den;
+endfunction
+
+%% this function returns the index of the columns in the dataset
+%% which seems to be more interesting than the others
+function I = selectColumns(X, threshold)
+    [~, cols] = size(X);
+    I = [];
+    index = 1;
+    for i = 1:cols
+        if (X(i) >= threshold)
+            I(index) = i;
+            index++;
+        endif
+    endfor
+    I(index) = cols+1;
+endfunction
